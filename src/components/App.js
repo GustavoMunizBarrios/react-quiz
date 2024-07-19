191bd1886f09d086e8b4f739364aa1c1390c2718
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-07-18 23:34:28 -0600

    Added Progress component to App.js and wrapped it with Question component when status is 'active'

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error'
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
@@ -72,7 +73,12 @@ export default function App() {
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
         {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
+        {status === 'active' && (
+          <>
+            <Progress index={index} numQuestions={numQuestions} />
+            <Question question={questions[index]} dispatch={dispatch} answer={answer} />
+          </>
+        )}
         <NextButton dispatch={dispatch} answer={answer} />
       </Main>
     </div>
