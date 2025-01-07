50fdebf6777d97a6b2dd4821869e3591d3dc446f
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2025-01-07 16:24:48 -0600

    `Added time limit and no time limit buttons to StartScreen component and updated CSS for button layout`

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,22 @@
 export default function StartScreen({ numQuestions, dispatch }) {
-    return (
-        <div className="start">
-            <h2>Welcome to The React Quiz!</h2>
-            <h3>{numQuestions} questions to test your React mastery</h3>
-            <button className="btn btn-ui" onClick={() => dispatch({ type: 'start' })}>Let's start</button>
-        </div>
-    )
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <div className="div-buttons">
+        <button className="btn btn-ui" onClick={() => dispatch({ type: "" })}>
+          Time Limit
+        </button>
+        <button className="btn btn-ui" onClick={() => dispatch({ type: "" })}>
+          No Time Limit
+        </button>
+      </div>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
+    </div>
+  );
 }
